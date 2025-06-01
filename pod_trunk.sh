c0d3147fd79f33759fe4c8b480ec35e827211b76
邵前前 <>
2025-06-01 23:45:08 +0800

    AdMoreNuSDK

--- pod_trunk.sh
+++ pod_trunk.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "AdMoreNuSDK"
 git push origin main
-git tag 3.4.6
+git tag 3.4.7
 git push origin main --tags
 pod trunk push AdMoreNuSDK.podspec --skip-import-validation --skip-tests --allow-warnings
